9ae5670fd23a9f95aa7981e4ea889b2f14911bdb
FaizanTabassum <97887943+FaizanTabassum@users.noreply.github.com>
2023-04-15 01:45:00 +0530

    this version now has an rtc module attached to the system so now the light will automatically turn on at 6 am everyday and turn off at 6pm even if the power goes out it will keep track of the time  and this is also a formatted version of the code

--- selfplanterfullsystemv2.ino
+++ selfplanterfullsystemv2.ino
@@ -1,12 +1,12 @@
 // all pins will be defined here
 #define DHTPIN 3
 const int LED_PIN = 13;
-const int mq135Pin = A0;  // Analog input pin connected to the MQ135 gas sensor
-int soilPin = A1;                       // soil moisture sensor pin
-int tempRelayPin = 11;                   // temperature relay pin
-int humRelayPin = 6;                    // humidity relay pin
-int airQualityRelayPin = 12;             // air quality relay pin
-int soilMoistureRelayPin = 10;           // soil moisture relay pin
+const int mq135Pin = A0;        // Analog input pin connected to the MQ135 gas sensor
+int soilPin = A1;               // soil moisture sensor pin
+int tempRelayPin = 11;          // temperature relay pin
+int humRelayPin = 6;            // humidity relay pin
+int airQualityRelayPin = 12;    // air quality relay pin
+int soilMoistureRelayPin = 10;  // soil moisture relay pin
 
 
 
@@ -24,35 +24,35 @@ int soilMoisture = 0;
 float temp = 0;
 
 // co2 sensor
-float ppm;                     // MQ-135 sensor pin
+float ppm;  // MQ-135 sensor pin
 
 // Define interval in milliseconds
-const unsigned long BLINK_INTERVAL = 12 * 60 * 60 * 1000UL; // 12 hours
+const unsigned long BLINK_INTERVAL = 12 * 60 * 60 * 1000UL;  // 12 hours
 
 // Variables to store time
 unsigned long previousMillis = 0;
 unsigned long currentMillis = 0;
 
 unsigned long startTimeforpump = 0;  // the time when the sensor value first fell below the threshold
-bool functionCalled = false;  // flag to indicate if the function has been called
+bool functionCalled = false;         // flag to indicate if the function has been called
 
 
 // LED state variable
 bool ledState = true;
 
-int tempThreshold = 25;                 // temperature threshold (in °C)
-int humThreshold = 40;                  // humidity threshold (in %)
-int airQualityThreshold = 1800;          // air quality threshold
-int soilMoistureThreshold = 10;        // soil moisture threshold
+int tempThreshold = 25;          // temperature threshold (in °C)
+int humThreshold = 40;           // humidity threshold (in %)
+int airQualityThreshold = 1800;  // air quality threshold
+int soilMoistureThreshold = 10;  // soil moisture threshold
 
 
 
 int N = 0;
 int P = 0;
 int K = 0;
-const int motor1Pin = 7;  // Pin connected to the motor 1 pump
-const int motor2Pin = 8;  // Pin connected to the motor 2 pump
-const int motor3Pin = 9;  // Pin connected to the motor 3 pump
+const int motor1Pin = 7;              // Pin connected to the motor 1 pump
+const int motor2Pin = 8;              // Pin connected to the motor 2 pump
+const int motor3Pin = 9;              // Pin connected to the motor 3 pump
 const float pumpRate = 5.0 / 1000.0;  // Pump rate in liters per second
 
 #include <Wire.h>
@@ -78,30 +78,30 @@ struct Plant {
 };
 
 Plant plants[] = {
-  {"Strawberry", 60, 70, 18.0, 400, "10-20-30"},
-  {"Basil", 50, 60, 20.0, 500, "3-1-2"},
-  {"Iceberg Lettuce", 60, 70, 16.0, 500, "10-10-10"},
-  {"Mint", 50, 60, 22.0, 450, "6-3-3"},
-  {"Spinach", 60, 70, 15.0, 500, "5-10-10"},
-  {"Rosemary", 40, 50, 18.0, 400, "10-6-8"},
-  {"Thyme", 50, 60, 18.0, 450, "5-10-10"},
-  {"Cilantro", 50, 60, 22.0, 500, "4-1-2"},
-  {"Chives", 50, 60, 18.0, 450, "5-10-5"},
-  {"Parsley", 50, 60, 20.0, 500, "5-10-10"},
-  {"Sage", 40, 50, 18.0, 400, "10-8-6"},
-  {"Lemon Balm", 50, 60, 22.0, 450, "5-10-5"},
-  {"Oregano", 40, 50, 18.0, 400, "10-8-6"},
-  {"Lavender", 40, 50, 18.0, 400, "5-10-10"},
-  {"Dill", 50, 60, 22.0, 500, "4-6-4"},
-  {"Celery", 60, 70, 15.0, 500, "5-10-10"},
-  {"Kale", 60, 70, 15.0, 500, "4-2-6"},
-  {"Arugula", 50, 60, 18.0, 500, "5-10-5"},
-  {"Cabbage", 60, 70, 15.0, 500, "5-10-10"},
-  {"Germination", 70, 60, 30.0, 500, "0-0-0"}
+  { "Strawberry", 60, 70, 18.0, 400, "10-20-30" },
+  { "Basil", 50, 60, 20.0, 500, "3-1-2" },
+  { "Iceberg Lettuce", 60, 70, 16.0, 500, "10-10-10" },
+  { "Mint", 50, 60, 22.0, 450, "6-3-3" },
+  { "Spinach", 60, 70, 15.0, 500, "5-10-10" },
+  { "Rosemary", 40, 50, 18.0, 400, "10-6-8" },
+  { "Thyme", 50, 60, 18.0, 450, "5-10-10" },
+  { "Cilantro", 50, 60, 22.0, 500, "4-1-2" },
+  { "Chives", 50, 60, 18.0, 450, "5-10-5" },
+  { "Parsley", 50, 60, 20.0, 500, "5-10-10" },
+  { "Sage", 40, 50, 18.0, 400, "10-8-6" },
+  { "Lemon Balm", 50, 60, 22.0, 450, "5-10-5" },
+  { "Oregano", 40, 50, 18.0, 400, "10-8-6" },
+  { "Lavender", 40, 50, 18.0, 400, "5-10-10" },
+  { "Dill", 50, 60, 22.0, 500, "4-6-4" },
+  { "Celery", 60, 70, 15.0, 500, "5-10-10" },
+  { "Kale", 60, 70, 15.0, 500, "4-2-6" },
+  { "Arugula", 50, 60, 18.0, 500, "5-10-5" },
+  { "Cabbage", 60, 70, 15.0, 500, "5-10-10" },
+  { "Germination", 70, 60, 30.0, 500, "0-0-0" }
 };
 
 const uint8_t num_plants = sizeof(plants) / sizeof(plants[0]);
-const uint8_t num_menu_items = 4; // Number of items that fit on the display at once
+const uint8_t num_menu_items = 4;  // Number of items that fit on the display at once
 
 uint8_t current_plant_index = 0;
 uint8_t top_menu_item_index = 0;
@@ -110,22 +110,22 @@ unsigned long last_debounce_time = 0;
 const unsigned long debounce_delay = 50;
 
 void setup() {
-  Serial.begin(9600);   
+  Serial.begin(9600);
   Wire.begin();
-  rtc.begin();                // initialize serial communication
-  pinMode(mq135Pin,INPUT);              // input for the mq135 pin
-  pinMode(soilPin,INPUT);               // input for the soil pin
-  pinMode(tempRelayPin, OUTPUT);        // set temperature relay pin as output
-  pinMode(humRelayPin, OUTPUT);         // set humidity relay pin as output
-  pinMode(airQualityRelayPin, OUTPUT);  // set air quality relay pin as output
-  pinMode(soilMoistureRelayPin, OUTPUT);// set soil moisture relay pin as output
-  
+  rtc.begin();                            // initialize serial communication
+  pinMode(mq135Pin, INPUT);               // input for the mq135 pin
+  pinMode(soilPin, INPUT);                // input for the soil pin
+  pinMode(tempRelayPin, OUTPUT);          // set temperature relay pin as output
+  pinMode(humRelayPin, OUTPUT);           // set humidity relay pin as output
+  pinMode(airQualityRelayPin, OUTPUT);    // set air quality relay pin as output
+  pinMode(soilMoistureRelayPin, OUTPUT);  // set soil moisture relay pin as output
+
   pinMode(motor1Pin, OUTPUT);
   pinMode(motor2Pin, OUTPUT);
   pinMode(motor3Pin, OUTPUT);
   pinMode(LED_PIN, OUTPUT);
 
-  dht.begin(); // initialize the sensor
+  dht.begin();  // initialize the sensor
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
   display.clearDisplay();
   display.setTextSize(1);
@@ -159,16 +159,16 @@ void drawMenu() {
 void printPlantData(Plant plant) {
   char* token = strtok(plant.npk_ratio, "-");
   N = atoi(token);
-  
+
   token = strtok(NULL, "-");
   P = atoi(token);
-  
+
   token = strtok(NULL, "-");
   K = atoi(token);
-  runWaterPumps(N,P,K);
-  tempThreshold = plant.temperature;                 // temperature threshold (in °C)
-  humThreshold = plant.humidity;                  // humidity threshold (in %)
-  airQualityThreshold = plant.co2_level;          // air quality threshold
+  runWaterPumps(N, P, K);
+  tempThreshold = plant.temperature;      // temperature threshold (in °C)
+  humThreshold = plant.humidity;          // humidity threshold (in %)
+  airQualityThreshold = plant.co2_level;  // air quality threshold
   soilMoistureThreshold = plant.soil_moisture;
   Serial.begin(9600);
   // Serial.print("Name: ");
@@ -192,13 +192,20 @@ void printPlantData(Plant plant) {
   display.setTextColor(SSD1306_WHITE);
   display.setCursor(0, 0);
   display.println(plant.name);
-  display.print("hum = ");display.print(plant.humidity);display.print("%");display.print("    sm = ");display.print(plant.soil_moisture);display.println("%");
-  display.print("Co2 = ");display.print(plant.co2_level);display.println("ppm");
-  display.print("temp = ");display.print(plant.temperature);display.println("C");
+  display.print("hum = ");
+  display.print(plant.humidity);
+  display.print("%");
+  display.print("    sm = ");
+  display.print(plant.soil_moisture);
+  display.println("%");
+  display.print("Co2 = ");
+  display.print(plant.co2_level);
+  display.println("ppm");
+  display.print("temp = ");
+  display.print(plant.temperature);
+  display.println("C");
   display.display();
   // display.startscrollleft(0x00, 0x07);
-
-
 }
 
 void loop() {
@@ -206,7 +213,7 @@ void loop() {
   blinkLed();
 
   // read temperature and humidity from DHT11 sensor
-  hum  = dht.readHumidity();
+  hum = dht.readHumidity();
   temp = dht.readTemperature();
 
   // read air quality from MQ135 sensor
@@ -215,8 +222,8 @@ void loop() {
   // read soil moisture from capacitive sensor
   soilMoisture = map(analogRead(soilPin), 750, 430, 0, 100);
 
-unsigned long current_time = millis();
- 
+  unsigned long current_time = millis();
+
   // Check if enough time has elapsed for button debounce
   if (current_time - last_debounce_time >= debounce_delay) {
     // Button debounce time has elapsed, so check button presses
@@ -249,72 +256,75 @@ unsigned long current_time = millis();
   delay(10);
 }
 
-void relaycontrol(){
+void relaycontrol() {
   static unsigned long lastMillis = 0;
   unsigned long currentMillis = millis();
 
-  if (currentMillis - lastMillis >= 2000) { 
+  if (currentMillis - lastMillis >= 2000) {
     lastMillis = currentMillis;
     // trigger temperature relay if threshold is exceeded
-  if(temp > tempThreshold){
-    digitalWrite(tempRelayPin, HIGH);
-    digitalWrite(airQualityRelayPin, HIGH);
-  }else{
-    digitalWrite(tempRelayPin, LOW);
-    digitalWrite(airQualityRelayPin, LOW);
-  }
-
-  // trigger humidity relay if threshold is exceeded
-  if(hum < humThreshold-5){
-  if(hum < humThreshold){
-    digitalWrite(humRelayPin, HIGH);
-  }else{
-    digitalWrite(humRelayPin, LOW);
-  }}
-  else{digitalWrite(humRelayPin, LOW);}
-
-  // trigger air quality relay if threshold is exceeded
-  if(ppm <= airQualityThreshold ){
-  if(ppm <= airQualityThreshold + 100){
-    digitalWrite(airQualityRelayPin, HIGH);
-  }else{
-    digitalWrite(airQualityRelayPin, LOW);
-  }}
-  else{digitalWrite(airQualityRelayPin, LOW);}
-
-  // trigger soil moisture relay if threshold is exceeded
-  
-
-// check if the sensor value is below the threshold
-if (soilMoisture < soilMoistureThreshold-10) {
- if (soilMoisture < soilMoistureThreshold) {
-    if (!functionCalled) {
-      // check if this is the first time the sensor value has fallen below the threshold
-      if (startTimeforpump == 0) {
-        startTimeforpump = millis();  // record the start time
-      } else {
-        // check if 10 seconds have passed since the start time
-        unsigned long currentTimeforpump = millis();
-        if (currentTimeforpump - startTimeforpump >= 60000) {
-          // 10 seconds have passed, call the function
-          waterover();
-        }
-      }
-      digitalWrite(soilMoistureRelayPin, HIGH);  // turn on the LED
+    if (temp > tempThreshold) {
+      digitalWrite(tempRelayPin, HIGH);
+      digitalWrite(airQualityRelayPin, HIGH);
+    } else {
+      digitalWrite(tempRelayPin, LOW);
+      digitalWrite(airQualityRelayPin, LOW);
     }
-  } else {
-    // reset the start time and functionCalled flag if the sensor value is above the threshold
-    startTimeforpump = 0;
-    functionCalled = false;
-    digitalWrite(soilMoistureRelayPin, LOW);  // turn off the LED
-  }}
-  else{digitalWrite(soilMoistureRelayPin, LOW);}
-    
-  
-  }
 
+    // trigger humidity relay if threshold is exceeded
+    if (hum < humThreshold - 5) {
+      if (hum < humThreshold) {
+        digitalWrite(humRelayPin, HIGH);
+      } else {
+        digitalWrite(humRelayPin, LOW);
+      }
+    } else {
+      digitalWrite(humRelayPin, LOW);
+    }
+
+    // trigger air quality relay if threshold is exceeded
+    if (ppm <= airQualityThreshold) {
+      if (ppm <= airQualityThreshold + 100) {
+        digitalWrite(airQualityRelayPin, HIGH);
+      } else {
+        digitalWrite(airQualityRelayPin, LOW);
+      }
+    } else {
+      digitalWrite(airQualityRelayPin, LOW);
+    }
+
+    // trigger soil moisture relay if threshold is exceeded
+
+
+    // check if the sensor value is below the threshold
+    if (soilMoisture < soilMoistureThreshold - 10) {
+      if (soilMoisture < soilMoistureThreshold) {
+        if (!functionCalled) {
+          // check if this is the first time the sensor value has fallen below the threshold
+          if (startTimeforpump == 0) {
+            startTimeforpump = millis();  // record the start time
+          } else {
+            // check if 10 seconds have passed since the start time
+            unsigned long currentTimeforpump = millis();
+            if (currentTimeforpump - startTimeforpump >= 60000) {
+              // 10 seconds have passed, call the function
+              waterover();
+            }
+          }
+          digitalWrite(soilMoistureRelayPin, HIGH);  // turn on the LED
+        }
+      } else {
+        // reset the start time and functionCalled flag if the sensor value is above the threshold
+        startTimeforpump = 0;
+        functionCalled = false;
+        digitalWrite(soilMoistureRelayPin, LOW);  // turn off the LED
+      }
+    } else {
+      digitalWrite(soilMoistureRelayPin, LOW);
+    }
+  }
 }
-void waterover(){
+void waterover() {
   functionCalled = true;
   startTimeforpump = 0;
   digitalWrite(soilMoistureRelayPin, LOW);
@@ -327,9 +337,9 @@ void waterover(){
   display.println("then reselect plant");
   display.display();
   digitalWrite(soilMoistureRelayPin, LOW);
-  while(true){}
-  }
-  
+  while (true) {}
+}
+
 void blinkLed() {
   DateTime now = rtc.now();
   int hour = now.hour();
@@ -337,19 +347,19 @@ void blinkLed() {
   int second = now.second();
 
   if (hour == 6 && minute == 0 && second == 0) {
-    digitalWrite(LED_PIN, HIGH); // turn on LED at 6 AM
+    digitalWrite(LED_PIN, HIGH);  // turn on LED at 6 AM
   }
   if (hour == 18 && minute == 0 && second == 0) {
-    digitalWrite(LED_PIN, LOW); // turn off LED at 6 PM
+    digitalWrite(LED_PIN, LOW);  // turn off LED at 6 PM
   }
 }
 void printSensorData() {
   static unsigned long lastMillis = 0;
   unsigned long currentMillis = millis();
 
-  if (currentMillis - lastMillis >= 2000) { // check if 5 seconds have elapsed
+  if (currentMillis - lastMillis >= 2000) {  // check if 5 seconds have elapsed
     lastMillis = currentMillis;
-    
+
     // Serial.print("Humidity: ");
     // Serial.println(hum);
     // Serial.print("Temperature: ");
@@ -358,8 +368,7 @@ void printSensorData() {
     // Serial.println(soilMoisture);
     // Serial.print("Co2 level: ");
     // Serial.println(ppm);
-    printf("%f-%f-%i-%f \n",hum,temp,soilMoisture,ppm);
-  
+    printf("%f-%f-%i-%f \n", hum, temp, soilMoisture, ppm);
   }
 }
 
@@ -398,25 +407,25 @@ void runWaterPumps(float nitrogenRatio, float phosphorusRatio, float potassiumRa
 
   // Start the timer
   unsigned long startTime = millis();
-  
+
   // Run the pumps for the required time to add the desired amount of solution
   while (millis() - startTime < (nitrogenTime + phosphorusTime + potassiumTime) * 1000) {
     if (millis() - startTime < nitrogenTime * 1000) {
       digitalWrite(motor1Pin, HIGH);  // Start the motor 1 pump
     } else {
-      digitalWrite(motor1Pin, LOW);   // Stop the motor 1 pump
+      digitalWrite(motor1Pin, LOW);  // Stop the motor 1 pump
     }
-    
+
     if (millis() - startTime < phosphorusTime * 1000) {
       digitalWrite(motor2Pin, HIGH);  // Start the motor 2 pump
     } else {
-      digitalWrite(motor2Pin, LOW);   // Stop the motor 2 pump
+      digitalWrite(motor2Pin, LOW);  // Stop the motor 2 pump
     }
-    
+
     if (millis() - startTime < potassiumTime * 1000) {
       digitalWrite(motor3Pin, HIGH);  // Start the motor 3 pump
     } else {
-      digitalWrite(motor3Pin, LOW);   // Stop the motor 3 pump
+      digitalWrite(motor3Pin, LOW);  // Stop the motor 3 pump
     }
   }
 
